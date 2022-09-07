48b6b0dcaa315c4fedf17ac7f2b7314962b0c894
김현교 <zkdlajt96@naver.com>
2022-09-08 00:01:07 +0900

    fix : es shard

--- src/main/java/class101/foo/io/Post.java
+++ src/main/java/class101/foo/io/Post.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 @Data
-@Document(indexName = "post")
+@Document(indexName = "post_shard_8")
 public class Post {
     @Id
     private String id;
